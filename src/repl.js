b35594ebd01cdb9f45600b42a4e3d3f4fb85b54a
Steve Kinney <hello@stevekinney.net>
2019-05-31 01:49:07 -0500

    Get the REPL and CLI set up

--- src/repl.js
+++ src/repl.js
@@ -3,6 +3,40 @@ const chalk = require('chalk');
 
 const { parseAndEvaluate } = require('./parse-and-evaluate');
 
-const repl = () => {};
+const askQuestions = () => {
+  const questions = [
+    {
+      name: 'COMMAND',
+      type: 'input',
+      message: chalk.blue('>'),
+    },
+  ];
+
+  return prompt(questions);
+};
+
+const repl = async () => {
+  try {
+    const answers = await askQuestions();
+    const { COMMAND } = answers;
+
+    if (COMMAND.trim()) {
+      console.log(chalk.yellow(parseAndEvaluate(COMMAND)));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  repl();
+};
+
+if (require.main === module) {
+  console.log(
+    chalk.red(
+      `Welcome to the ${chalk.bgYellow('Dropbear')} Programming Language`,
+    ),
+  );
+  repl();
+}
 
 module.exports = repl;
