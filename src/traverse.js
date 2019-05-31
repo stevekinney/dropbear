2478b9f2cb14c6daeb25e59d4e62d9f52d26411b
Steve Kinney <hello@stevekinney.net>
2019-05-31 02:25:29 -0500

    Add AST traversal

--- src/traverse.js
+++ src/traverse.js
@@ -1,3 +1,27 @@
-const traverse = () => {};
+const traverseArray = ({ array, parent, visitor }) => {
+  array.forEach(node => {
+    traverseNode({ node, parent, visitor });
+  });
+};
+
+const traverseNode = ({ node, parent, visitor }) => {
+  let methods = visitor[node.type];
+
+  if (methods && methods.enter) {
+    methods.enter({ node, parent });
+  }
+
+  if (node.arguments) {
+    traverseArray({ array: node.arguments, parent: node, visitor });
+  }
+
+  if (methods && methods.exit) {
+    methods.exit({ node, parent });
+  }
+};
+
+const traverse = (node, visitor) => {
+  traverseNode({ node, visitor });
+};
 
 module.exports = { traverse };
--- tests/traverse.test.js
+++ tests/traverse.test.js
@@ -1,7 +1,7 @@
 const { traverse } = require('../src/traverse');
 
 describe(traverse, () => {
-  it.skip('should travel to all the nodes in the tree', () => {
+  it('should travel to all the nodes in the tree and reverse the math', () => {
     const ast = {
       type: 'CallExpression',
       name: 'add',
@@ -29,5 +29,29 @@ describe(traverse, () => {
     traverse(ast, visitor);
 
     expect(ast.name).toBe('subtract');
+  });
+
+  it('should travel to all the nodes in the tree and double all of the numbers', () => {
+    const ast = {
+      type: 'CallExpression',
+      name: 'add',
+      arguments: [
+        { type: 'NumericLiteral', value: 12 },
+        { type: 'NumericLiteral', value: 6 },
+      ],
+    };
+
+    const visitor = {
+      NumericLiteral: {
+        exit({ node }) {
+          node.value = node.value * 2;
+        },
+      },
+    };
+
+    traverse(ast, visitor);
+
+    expect(ast.arguments[0].value).toBe(24);
+    expect(ast.arguments[1].value).toBe(12);
   });
 });
