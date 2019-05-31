f76d00c880bf67984f5b63f272e9dcbb28f4129d
Steve Kinney <hello@stevekinney.net>
2019-05-31 10:20:23 -0500

    Get to exercise 1

--- src/tokenize.js
+++ src/tokenize.js
@@ -6,6 +6,29 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+const tokenize = input => {
+  const tokens = [];
+  let cursor = 0;
+
+  while (cursor < input.length) {
+    const character = input[cursor];
+
+    if (isParenthesis(character)) {
+      tokens.push({
+        type: 'Parenthesis',
+        value: character,
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(character)) {
+      cursor++;
+      continue;
+    }
+  }
+
+  return tokens;
+};
 
 module.exports = { tokenize };
--- tests/tokenize.test.js
+++ tests/tokenize.test.js
@@ -1,11 +1,11 @@
 import { tokenize } from '../src/tokenize';
 
 describe(tokenize, () => {
-  it.skip('should return an array', () => {
+  it('should return an array', () => {
     expect(Array.isArray(tokenize(''))).toBe(true);
   });
 
-  it.skip('should be able to tokenize a pair of parentheses', () => {
+  it('should be able to tokenize a pair of parentheses', () => {
     const input = '()';
     const result = [
       { type: 'Parenthesis', value: '(' },
@@ -15,7 +15,7 @@ describe(tokenize, () => {
     expect(tokenize(input)).toEqual(result);
   });
 
-  it.skip('should ignore whitespace completely', () => {
+  it('should ignore whitespace completely', () => {
     const input = '                  ';
     const result = [];
 
